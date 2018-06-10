a73622e8d78d7f51db135e707dd85380b2473665
Ross Williams <r.b.williams94@gmail.com>
2018-06-10 16:20:55 +0100

    Updates to npm and config

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -3,10 +3,10 @@ import * as firebase from "firebase";
 export default () => {
   const config = {
     apiKey: "AIzaSyD6H8FnFKWxKKfYW0GR9l5Fxgwj9MIkQ4g",
-    authDomain: "beezer-test-fb095.firebaseapp.com",
-    databaseURL: "https://beezer-test-fb095.firebaseio.com",
-    projectId: "beezer-test-fb095",
-    storageBucket: "beezer-test-fb095.appspot.com",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
     messagingSenderId: "97896488228"
   };
   firebase.initializeApp(config);
